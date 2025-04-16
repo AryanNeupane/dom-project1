9bbeaa627521b6e67fd45b04e80259a0f53c6874
Aryan Neupane <official.aryanneupane@gmail.com>
2025-04-16 16:55:13 +0545

    final comit

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+let stat = document.querySelector("h5");
+let add = document.querySelector("#add");
+let flag = 0;
 
-let stat= document.querySelector("h5")
-let add=document.querySelector("#add")
-varflag=0
-add.addEventListener("click",()=>{
-    flag=1
-    stat.innerHTML="Friends";
-    stat.style.color="green"
-    add.innerHTML="Unfriend"
-    add.style.backgroundColor="Red"
-    add.classList.add("unfriend")
-    
-})
-let unf=document.querySelector("unfriend")
-
-unf.addEventListener("click",()=>{
-     
-})
+add.addEventListener("click", () => {
+    if (flag === 0) {
+        stat.innerHTML = "Friends";
+        stat.style.color = "green";
+        add.innerHTML = "Unfriend";
+        add.style.backgroundColor = "red";
+        add.classList.add("unfriend");
+        flag = 1;
+    } else {
+        stat.innerHTML = "Stranger";
+        stat.style.color = "red";
+        add.innerHTML = "Add Friend";
+        add.style.backgroundColor = "cadetblue";
+        add.classList.remove("unfriend");
+        flag = 0;
+    }
+});
